79e1b39cee6c8ed0c76ab823e50d50ef94554e08
NilsFo <nils.foerster@yahoo.de>
2023-07-09 18:44:57 +0200

    last minute push

--- Assets/Scripts/SoundTrigger.cs
+++ Assets/Scripts/SoundTrigger.cs
@@ -3,12 +3,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SoundTrigger : MonoBehaviour {
+public class SoundTrigger : MonoBehaviour
+{
     public GameState gameState;
     public AudioClip audioClip;
-    
-    private void OnTriggerEnter2D(Collider2D other) {
-        if(other.CompareTag("Player"))
+    public GameObject leafsPoof;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
             gameState.musicManager.CreateAudioClip(audioClip, 0.4f, true);
+            Instantiate(leafsPoof, transform.position, Quaternion.identity);
+        }
     }
 }
